21735ae660269c254554b668e00d96abc7b1d399
saadetcatakakgunes1211 <saadetcatak1453@gmail.com>
2023-06-04 21:47:13 +0300

    Add project files.

---
+++ InspimoApi/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InspimoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+         {
+            return Ok();
+         }
+
+        [HttpPost]
+        public IActionResult AddCategory()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteCategory()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult PutCategory()
+        {
+            return Ok();
+        }
+    }
+}
